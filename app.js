edd6e814c39a0c5041ebce5c2ccffa6fee0662f2
faith-cyber <dammyminaj@gmail.com>
2021-07-08 17:08:43 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// swt initial count
+let count = 0;
+
+// select value and buutons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    }
+    else if (styles.contains('increase')) {
+      count++;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#333333";
+    }
+    value.textContent = count;
+  });
+});
